06c11ae2826e875c06992d0f381536f5b7d33d4f
lovebirdegg <lovebirdegg@gmail.com>
2025-02-13 09:30:15 +0800

    Update sd.py

--- sd.py
+++ sd.py
@@ -6,6 +6,52 @@ import json
 import notify
 import os  # 用于导入系统变量
 from datetime import date
+push_config = {
+    'HITOKOTO': False,                  # 启用一言（随机句子）
+
+    'BARK_PUSH': 'https://api.day.app/h9pruUjgra99mHLV3TiRJB',                    # bark IP 或设备码，例：https://api.day.app/DxHcxxxxxRxxxxxxcm/
+    'BARK_ARCHIVE': '',                 # bark 推送是否存档
+    'BARK_GROUP': '',                   # bark 推送分组
+    'BARK_SOUND': '',                   # bark 推送声音
+    'BARK_ICON': '',                    # bark 推送图标
+}
+def bark(title: str, content: str) -> None:
+    """
+    使用 bark 推送消息。
+    """
+    if not push_config.get("BARK_PUSH"):
+        print("bark 服务的 BARK_PUSH 未设置!!\n取消推送")
+        return
+    print("bark 服务启动")
+
+    if push_config.get("BARK_PUSH").startswith("http"):
+        url = f'{push_config.get("BARK_PUSH")}/{urllib.parse.quote_plus(title)}/{urllib.parse.quote_plus(content)}'
+    else:
+        url = f'https://api.day.app/{push_config.get("BARK_PUSH")}/{urllib.parse.quote_plus(title)}/{urllib.parse.quote_plus(content)}'
+
+    bark_params = {
+        "BARK_ARCHIVE": "isArchive",
+        "BARK_GROUP": "group",
+        "BARK_SOUND": "sound",
+        "BARK_ICON": "icon",
+    }
+    params = ""
+    for pair in filter(
+        lambda pairs: pairs[0].startswith("BARK_")
+        and pairs[0] != "BARK_PUSH"
+        and pairs[1]
+        and bark_params.get(pairs[0]),
+        push_config.items(),
+    ):
+        params += f"{bark_params.get(pair[0])}={pair[1]}&"
+    if params:
+        url = url + "?" + params.rstrip("&")
+    response = requests.get(url).json()
+
+    if response["code"] == 200:
+        print("bark 推送成功！")
+    else:
+        print("bark 推送失败！")
 
 def dingding_bot(title: str, content: str) -> None:
     """
@@ -104,11 +150,11 @@ def fetch_procurement_info():
         if(len(results) > 1):
             markdown_table = convert_table_to_markdown(results)
             dingding_bot("招标信息",markdown_table)
-        notify.bark("sfy_sucess","运行成功")
+        bark("sfy_sucess","运行成功")
         print("运行成功")
 
     except Exception as e:
-        notify.bark("sfy_error",f"抓取失败: {str(e)}")
+        bark("sfy_error",f"抓取失败: {str(e)}")
         print(f"抓取失败: {str(e)}")
 
 if __name__ == "__main__":
